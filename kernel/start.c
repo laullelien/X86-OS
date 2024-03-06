a2cac558c32f5638c79b6acd44e9b7d0a6865485
Lucien Reboul <Lucien.Reboul@grenoble-inp.org>
2024-03-06 12:39:04 +0100

    test3 KO

--- kernel/start.c
+++ kernel/start.c
@@ -181,32 +181,100 @@ int test2(void *arg)
 
 
 //Test 3
-//TODO
+
+int prio4(void *arg)
+{
+        /* arg = priority of this proc. */
+        int r;
+
+        assert(getprio(getpid()) == (int) arg);
+        printf("1");
+        r = chprio(getpid(), 64);
+        assert(r == (int) arg);
+        printf(" 3");
+        return 0;
+}
+
+int prio5(void *arg)
+{
+        /* Arg = priority of this proc. */
+        int r;
+
+        assert(getprio(getpid()) == (int) arg);
+        printf(" 7");
+        r = chprio(getpid(), 64);
+        assert(r == (int)arg);
+        printf("error: I should have been killed\n");
+        assert(0);
+        return 0;
+}
+
+
+int test3(void *arg)
+{
+        int pid1;
+        int p = 192;
+        int r;
+
+        (void)arg;
+
+        assert(getprio(getpid()) == 128);
+        pid1 = start(prio4, 4000, p, "prio4",(void *) p);
+        assert(pid1 > 0);
+        printf(" 2");
+        r = chprio(getpid(), 32);
+        assert(r == 128);
+        printf(" 4");
+        r = chprio(getpid(), 128);
+        assert(r == 32);
+        printf(" 5");
+        assert(waitpid(pid1, 0) == pid1);
+        printf(" 6");
+
+        assert(getprio(getpid()) == 128);
+        pid1 = start(prio5, 4000, p, "prio5",(void *) p);
+        assert(pid1 > 0);
+        printf(" 8");
+        r = kill(pid1);
+        assert(r == 0);
+        assert(waitpid(pid1, 0) == pid1);
+        printf(" 9");
+        r = chprio(getpid(), 32);
+        assert(r == 128);
+        printf(" 10");
+        r = chprio(getpid(), 128);
+        assert(r == 32);
+        printf(" 11.\n");
+        return 0; //Added
+}
 
 //////
 
 
 
-int start_test(void*    ) {
-
-        
+int start_test(void* ) {
         int pid;
         int ret;
 
 	printf("Test %i : ", 0);
-	pid = start(test0, 4000,1,"test0",NULL);
+	pid = start(test0, 4000,128,"test0",NULL);
         printf("pid = %i",pid);
 	waitpid(pid, &ret);
 	assert(ret == 0);
 
 	printf("Test %i : ", 1);
-	pid = start(test1, 4000,1,"test1",NULL);
+	pid = start(test1, 4000,128,"test1",NULL);
 	waitpid(pid, &ret);
 	assert(ret == 0);
 
 
 	printf("Test %i : ", 2);
 	pid = start(test2, 4000,128,"test2",NULL);
+	waitpid(pid, &ret);
+	assert(ret == 0);
+
+        printf("Test %i : ", 3);
+	pid = start(test3, 4000,128,"test3",NULL);
 	waitpid(pid, &ret);
 	assert(ret == 0);
         
