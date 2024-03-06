08313666580197fe3a52d531f21ff5df13a3201a
Sebastien Wyckaert <sebastien.wyckaert@grenoble-inp.org>
2024-03-06 11:22:47 +0100

    Base pour phase 4
    Ajout des fichiers pour la synchro

---
+++ kernel/synchro.h
@@ -0,0 +1,15 @@
+#ifndef __SYNCHRO_H__
+#define __SYNCHRO_H__
+
+/*
+
+    pcreate : crée une file de messages
+    pdelete : détruit une file de messages
+    psend : dépose un message dans une file
+    preceive : retire un message d'une file
+    preset : réinitialise une file
+    pcount : renvoie l'état courant d'une file
+
+*/
+
+#endif
