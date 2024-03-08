5f83e3af2bcb5e0e9bfe73b9a3625758ed259e06
Lucien Reboul <Lucien.Reboul@grenoble-inp.org>
2024-03-08 09:29:57 +0100

    test6 et update kill()

--- kernel/launchtest.c
+++ kernel/launchtest.c
@@ -191,11 +191,197 @@ int test3(void *arg)
 
 /*  Test 4  */
 
+void test_it(void)
+{
+#ifdef microblaze
+        int status, mstatus;
+        __asm__ volatile("mfs %0,rmsr; ori %1,%0,2; mts rmsr,%1; nop; nop; mts rmsr,%0":"=r" (status), "=r" (mstatus));
+#else
+        __asm__ volatile("pushfl; testl $0x200,(%%esp); jnz 0f; sti; nop; cli; 0: addl $4,%%esp\n":::"memory");
+#endif
+}
 
 
-#define NB_TEST_CASE 4
+int busy1(void *arg)
+{
+        (void)arg;
+        while (1) {
+                int i, j;
+
+                printf(" A");
+                for (i=0; i<loop_count1; i++) {
+                        test_it();
+                        for (j=0; j<loop_count0; j++);
+                }
+        }
+        return 0;
+}
+/* assume the process to suspend has a priority == 64 */
+int busy2(void *arg)
+{
+        int i;
+
+        for (i = 0; i < 3; i++) {
+                int k, j;
+
+                printf(" B");
+                for (k=0; k<loop_count1; k++) {
+                        test_it();
+                        for (j=0; j<loop_count0; j++);
+                }
+        }
+        i = chprio((int) arg, 16);
+        assert(i == 64);
+        return 0;
+}
+
+int test4(void *args)
+{
+        int pid1, pid2;
+        int r;
+        int arg = 0;
+
+        (void)args;
+
+        assert(getprio(getpid()) == 128);
+        pid1 = start(busy1, 4000, 64, "busy1",(void *) arg);
+        assert(pid1 > 0);
+        pid2 = start(busy2, 4000, 64, "busy2", (void *) pid1);
+        assert(pid2 > 0);
+        printf("1 -");
+        r = chprio(getpid(), 32);
+        assert(r == 128);
+        printf(" - 2");
+        r = kill(pid1);
+        assert(r == 0);
+        assert(waitpid(pid1, 0) == pid1);
+        r = kill(pid2);
+        assert(r < 0); /* kill d'un processus zombie */
+        assert(waitpid(pid2, 0) == pid2);
+        printf(" 3");
+        r = chprio(getpid(), 128);
+        assert(r == 32);
+        printf(" 4.\n");
+
+        return 0;
+}
+
+/* Test 6 */
+
+// int proc6_1(void){
+//         #if defined microblaze
+//         __asm__(
+//         ".text\n"
+//         ".globl main\n"
+//         "main:\n"
+//         "addik r3,r0,3\n"
+//         "rtsd r15,8\n"
+//         "nop\n"
+//         ".previous\n"
+//         );
+//         #else
+//         __asm__(
+//         ".text\n"
+//         ".globl main\n"
+//         "main:\n"
+//         "movl $3,%eax\n"
+//         "ret\n"
+//         ".previous\n"
+//         );
+//         #endif
+//         return 0;
+// }
+
+// int proc6_2(void){
+//         #if defined microblaze
+//         __asm__(
+//         ".text\n"
+//         ".globl main\n"
+//         "main:\n"
+//         "addk r3,r0,r5\n"
+//         "swi r3,r1,-4\n"
+//         "rtsd r15,8\n"
+//         "nop\n"
+//         ".previous\n"
+//         );
+//         #else
+//         __asm__(
+//         ".text\n"
+//         ".globl main\n"
+//         "main:\n"
+//         "movl 4(%esp),%eax\n"
+//         "pushl %eax\n"
+//         "popl %eax\n"
+//         "ret\n"
+//         ".previous\n"
+//         );
+//         #endif
+//         return 0;
+// }
+
+// int proc6_3(void){
+//         #if defined microblaze
+//         __asm__(
+//         ".text\n"
+//         ".globl main\n"
+//         "main:\n"
+//         "addk r3,r0,r5\n"
+//         "swi r3,r1,-4\n"
+//         "rtsd r15,8\n"
+//         "nop\n"
+//         ".previous\n"
+//         );
+//         #else
+//         __asm__(
+//         ".text\n"
+//         ".globl main\n"
+//         "main:\n"
+//         "movl 4(%esp),%eax\n"
+//         "pushl %eax\n"
+//         "popl %eax\n"
+//         "ret\n"
+//         ".previous\n"
+//         );
+//         #endif
+//         return 0;
+// }
+
+
+// int test6(void *arg)
+// {
+//         int pid1, pid2, pid3;
+//         int ret;
+
+//         (void)arg;
+
+//         assert(getprio(getpid()) == 128);
+//         pid1 = start(proc6_1, 0, 64, "proc6_1", 0);
+//         assert(pid1 > 0);
+//         pid2 = start(proc6_2, 4, 66, "proc6_2", (void*)4);
+//         assert(pid2 > 0);
+//         pid3 = start(proc6_3, 0xffffffff, 65, "proc6_3",(void*)5);
+//         assert(pid3 < 0);
+//         pid3 = start(proc6_3, 8, 65, "proc6_3",(void*)5);
+//         assert(pid3 > 0);
+//         assert(waitpid(-1, &ret) == pid2);
+//         assert(ret == 4);
+//         assert(waitpid(-1, &ret) == pid3);
+//         assert(ret == 5);
+//         assert(waitpid(-1, &ret) == pid1);
+//         assert(ret == 3);
+//         assert(waitpid(pid1, 0) < 0);
+//         assert(waitpid(-1, 0) < 0);
+//         assert(waitpid(getpid(), 0) < 0);
+//         printf("ok.\n");
+// }
+
+
+
+////
+
+#define NB_TEST_CASE 5
 static int size = NB_TEST_CASE;
-static int (*test_case[NB_TEST_CASE])(void *) = {test0, test1, test2, test3};
+static int (*test_case[NB_TEST_CASE])(void *) = {test0, test1, test2, test3, test4};
 
 int launchtest() {
     int pid;
--- kernel/launchtest.h
+++ kernel/launchtest.h
@@ -7,6 +7,19 @@
 #ifndef __LAUNCHTEST_H__
 #define __LAUNCHTEST_H__
 
+//Const for tests:
+#define DUMMY_VAL 78
+
+//Test 4 
+#ifdef microblaze
+static const int loop_count0 = 500;
+static const int loop_count1 = 1000;
+#else
+static const int loop_count0 = 5000;
+static const int loop_count1 = 10000;
+#endif
+
+
 extern int launchtest();
 
 #endif
