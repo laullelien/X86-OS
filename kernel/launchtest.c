80bac1fb532f030728dedcc301463b9363d5b941
Simon Dussud <simon.dussud@grenoble-inp.org>
2024-03-13 12:31:00 +0100

    portable test 7 added

--- kernel/launchtest.c
+++ kernel/launchtest.c
@@ -314,7 +314,7 @@ int test5(void *arg)
         assert(r == 0);
         printf("ok.\n");
 
-        return 1;   //added
+        return 0;   //added
 }
 
 /* Test 6 */
@@ -430,9 +430,121 @@ int test5(void *arg)
 
 ////
 
+int sleep_pr1(void *arg)
+{
+        (void)arg;
+        wait_clock(current_clock() + 2);
+        printf(" not killed !!!");
+        assert(0);
+        return 1;
+}
+
+int timer(void *arg)
+{
+        volatile unsigned long *timer = NULL;
+        timer = shm_acquire("test7_shm");
+        assert(timer != NULL);
+
+        (void)arg;
+        while (1) {
+                unsigned long t = *timer + 1;
+                *timer = t;
+                while (*timer == t) test_it();
+        }
+        while (1);
+        return 0;
+}
+
+int timer1(void *arg)
+{
+        (void)arg;
+
+        unsigned long quartz;
+        unsigned long ticks;
+        unsigned long dur;
+        int i;
+
+        clock_settings(&quartz, &ticks);
+        dur = (quartz + ticks) / ticks;
+        printf(" 2");
+        for (i = 4; i < 8; i++) {
+                wait_clock(current_clock() + dur);
+                printf(" %d", i);
+        }
+        return 0;
+}
+
+
+/*******************************************************************************
+ * Test 7
+ *
+ * Test de l'horloge (ARR et ACE)
+ * Tentative de determination de la frequence du processeur et de la
+ * periode de scheduling
+ ******************************************************************************/
+
+#ifdef TELECOM_TST
+int test7(void *arg)
+{
+        (void)arg;
+        printf("Test desactive pour les TELECOM.\n");
+}
+
+#else
+
+int test7(void *arg)
+{
+        int pid1, pid2, r;
+        unsigned long c0, c, quartz, ticks, dur;
+        volatile unsigned long *timer = NULL;
+
+        (void)arg;
+        timer = shm_create("test7_shm");
+        assert(timer != NULL);
+
+        assert(getprio(getpid()) == 128);
+        printf("1");
+        pid1 = start(timer1, 4000, 129, "timer1", 0);
+        assert(pid1 > 0);
+        printf(" 3");
+        assert(waitpid(-1, 0) == pid1);
+        printf(" 8 : ");
+
+        *timer = 0;
+        pid1 = start(timer, 4000, 127, "timer", 0);
+        pid2 = start(timer, 4000, 127, "timer", 0);
+        assert(pid1 > 0);
+        assert(pid2 > 0);
+        clock_settings(&quartz, &ticks);
+        dur = 2 * quartz / ticks;
+        test_it();
+        c0 = current_clock();
+        do {
+                test_it();
+                c = current_clock();
+        } while (c == c0);
+        wait_clock(c + dur);
+        assert(kill(pid1) == 0);
+        assert(waitpid(pid1, 0) == pid1);
+        assert(kill(pid2) == 0);
+        assert(waitpid(pid2, 0) == pid2);
+        printf("%lu changements de contexte sur %lu tops d'horloge", *timer, dur);
+        pid1 = start("sleep_pr1", 4000, 192, 0);
+        assert(pid1 > 0);
+        assert(kill(pid1) == 0);
+        assert(waitpid(pid1, &r) == pid1);
+        assert(r == 0);
+        printf(".\n");
+        shm_release("test7_shm");
+}
+#endif
+
+
+
+
 #define NB_TEST_CASE 6
 static int size = NB_TEST_CASE;                 /*Mark null to not execute the test case*/
-static int (*test_case[NB_TEST_CASE])(void *) = {test0, test1, test2, test3, test4, test5};
+static int (*test_case[NB_TEST_CASE])(void *) = {test0, test1, test2, test3, test4, test5, NULL, test7};
 
 int launchtest() {
     int pid;
