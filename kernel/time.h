ca1e4c91d3bd1d87545cafb2530ac56482da1c8f
Sebastien Wyckaert <sebastien.wyckaert@grenoble-inp.org>
2024-03-13 11:25:59 +0100

    Ajout de fonctions en rapport avec la clock
    - current_clock
    - clock_settings

--- kernel/time.h
+++ kernel/time.h
@@ -4,13 +4,13 @@
 #include <stdint.h>
 
 
-
-//extern uint32_t nb_tic;
-
 extern void traitant_IT_32 (void);
 
 void tic_PIT(void);
 
 void init_clock(void);
+
+unsigned long current_clock();
+void clock_settings(unsigned long *quartz, unsigned long *ticks);
 
 #endif
